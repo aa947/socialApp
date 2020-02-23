e6367ed81b23925e993e5285fc3c7c88c1bcd517
Ahmad Ali <aallii300300@gmail.com>
2020-02-23 17:54:35 +0000

    Navbar started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+
+//import components
+import Navbar from './components/Navbar';
+
+//import pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
 
 function App() {
   return (
     <div className="App">
-      <h1> SocialApp </h1>
+  <Router>
+  <Navbar />
+    <div className="container">
+    <Switch>
+      <Route exact path='/' component={home}  />
+      <Route exact path='/login' component={login}  />
+      <Route exact path='/signup' component={signup}  />
+    </Switch>
+    </div>
+  </Router>
+
     </div>
   );
 }
